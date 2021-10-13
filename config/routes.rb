23bb3cde7351dc336b18feea2fe3cf7188538ebb
fujitamariko <fujyta.mrk@gmail.com>
2021-10-13 17:57:24 +0900

    viewとcontroller実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#new'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'sign_up', to: "users/registrations#new"
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
