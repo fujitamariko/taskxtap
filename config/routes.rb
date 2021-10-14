e11180fdf4afe83506e82f66cc7b8e9bb8e4086e
fujitamariko <fujyta.mrk@gmail.com>
2021-10-14 19:54:49 +0900

    ヘッダー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   resources :tasks
-  resources :users, only: [:show]
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'tops#index'
 
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_up', to: "users/registrations#new"
   end
+  resources :users, only: [:show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
