f45b85a8fe3588a2f2ccc8427b93b5f5621dd478
fujitamariko <fujyta.mrk@gmail.com>
2021-10-14 19:41:01 +0900

    Userのshowページ・ヘッダー・フッター実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :tasks
+  resources :users, only: [:show]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'tops#index'
 
