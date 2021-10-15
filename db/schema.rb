d0eca8bdd1ff9636306227d2136b206e76b8dd9c
fujitamariko <fujyta.mrk@gmail.com>
2021-10-15 10:50:58 +0900

    自分が作成したタスクだけ表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_155554) do
+ActiveRecord::Schema.define(version: 2021_10_14_232726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_10_14_155554) do
     t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +79,5 @@ ActiveRecord::Schema.define(version: 2021_10_14_155554) do
   add_foreign_key "comments", "users"
   add_foreign_key "labellings", "labels"
   add_foreign_key "labellings", "tasks"
+  add_foreign_key "tasks", "users"
 end
