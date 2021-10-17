d24f833facfdda7cc8fcef35adcef0fd5f239cdd
fujitamariko <fujyta.mrk@gmail.com>
2021-10-17 20:51:05 +0900

    ソートボタンの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks.all.page(params[:page]).per(5)
     @q = @tasks.ransack(params[:q])
-    @tasks = @q.result.includes(:user).page(params[:page]).order("deadline_on asc").per(5)
+    @tasks = @tasks.order("deadline_on asc") if params[:sort_limit]
     @tasks = @tasks.joins(:labels).where(labels: { id: params[:label_id] }) if params[:label_id].present?
   end
 
